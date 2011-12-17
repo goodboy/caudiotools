b30d9fd942df08917a369bfe0a5bddad54ab16ca
Tyler Goodlet <tgoodlet@gmail.com>
2011-12-17 18:10:14 -0500

    made some progress

--- xcorr.c
+++ xcorr.c
@@ -9,20 +9,42 @@
 ((char [])a)[0] == 0xff
 ((char *)a)[0] == 0xff <-- most */
 
-/*x[i + 0] << 24 || x[i + 1] << 16 || x[i + 2] << 8 || x[i + 3] / (double)(2 << bitdepth)
-    for(...) {
-        accum ||= *++x;
-        accum <<= 8
-    }
-accum / (double)(2 << bitdepth)*/
 
-int
-xrr(int maxdelay, buffer_t *sig1, buffer_t *sig2)
+double *
+xcorr(int maxdelay, buffer_t *sig1, buffer_t *sig2)
 {
-int i,j 
-   double mx,my,sx,sy,sxy,denom,r;
-    x = sig1->pcm[0];
-    y = sig2->pcm[0];
+    //THE SHITTY O(n^2) way! (i.e. not using fft)
+    int i,j; 
+    double mx,my,sx,sy,sxy,denom;
+    double wlength;
+
+    double *x;// = sig1->vector[0];
+    double *small;
+    double *big;
+    double *R;
+
+    /* hack to match vector lengths by zero padding */
+    if(sig1->length >= sig2->length) {
+        wlength = sig1->length;
+        small = sig2->vector[0];
+        big = sig1->vector[0];
+    }else{
+        wlength = sig2->length;
+        small = sig1->vector[0];
+        big = sig2->vector[0];
+    }
+
+    double y[(int)wlength];
+    //y = calloc(wlength, sizeof(double));
+    for(i = 0; i < wlength; i++) {
+        y[i] = small[i];
+
+
+
+    if(maxdelay == 0)
+        maxdelay = 2*wlength;
+    
+    R = calloc(2*wlength - 1, sizeof(double));
 
     /*calculate the mean of the two series x[], y[] */
     mx = 0;
@@ -43,13 +65,13 @@ int i,j
         sx += (x[i] - mx) * (x[i] - mx);
         sy += (y[i] - my) * (y[[i] - my);
     }
-    denom = sqrt(sx*sy); 
+    denom = sqrt(sx*sy);
 
     /* Calculate the correlation series */
     
-    for (delay = -maxdelay; delay < maxdelay; delay++;){
+    for (delay = -maxdelay; delay < maxdelay; delay++;) {
         sxy=0;
-        for(i = 0; i < n; i ++){
+        for(i = 0; i < n; i ++) {
             j = i + delay;
             if(j < 0 || j >= n)
                 continue;
@@ -57,18 +79,18 @@ int i,j
             else
                 sxy += (x[i] - mx) * (y[j] - my);
         } 
-        r[delay] = sxy / denom;
-
+        R[delay] = sxy / denom;
+    return R;
 }
 
     /* Calc the corr series - cirular boundary conditions 
     for (delay = -maxdelay; delay < maxdelay; delay++;)
         sxy=0;
-        for(i = 0; i < n; i ++){
+        for(i = 0; i < n; i ++) {
             j = i + delay;
             while(j < 0)
                 j += n;
             j %= n;
             sxy += (x[i] - mx) * (y[j] - my);
         }
-        r = sxy / denom;*/
+        R = sxy / denom;*/
